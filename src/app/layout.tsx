83f5a07195835d1cd92ac8d937ab2102de308047
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-11-07 15:29:36 -0500

    chore: updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+
+import type { Metadata } from "next";
+
+import "./globals.css";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
